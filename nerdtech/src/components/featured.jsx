d934d772c5b1429751595decd32f785c6930776f
rushi-bhaskar <rushikeshbhaskar7@gmail.com>
2023-11-06 14:30:10 +0530

    ask for qoute navigation

--- nerdtech/src/components/featured.jsx
+++ nerdtech/src/components/featured.jsx
@@ -1,29 +1,48 @@
-import React from 'react'
+import React from "react";
 // import vector from '../assets/Background.png'
-import vector from '../assets/vector.svg'
-import Count from './count';
+import vector from "../assets/vector.svg";
+import Count from "./count";
 
 const Featured = () => {
-  return (<>
-  
-    <div className='flex justify-between items-start w-full py-10 px-24 pt-28 border-solid  ' style={{ fontFamily:'nothing'}}>
+  return (
+    <>
+      <div
+        className="flex justify-between items-start w-full py-10 px-24 pt-28 border-solid  "
+        style={{ fontFamily: "nothing" }}
+      >
         <div className=" flex flex-col items-start pl-12 pt-14 h-full w-[50%] gap-5 text-white border-solid  ">
-            <div className="text-4xl " style={{ fontSize: 55, letterSpacing:2, lineHeight:1 }}>We create ideas into <span className='text-[#f5e40b]'>real Products</span> </div>
-            <div className="text-lg" style={{ fontFamily:'nothing'}}>We make your ideas come to life with the latest technology and assurred quality</div>
-            <button className=' bg-[#f5e40b] py-3 px-10 rounded-md text-black text-lg'>ASK FOR QOUTE</button>
-            <div className="pt-8 pl-16">
-          <Count/>
-        </div>
+          <div
+            className="text-4xl "
+            style={{ fontSize: 55, letterSpacing: 2, lineHeight: 1 }}
+          >
+            We create ideas into{" "}
+            <span className="text-[#f5e40b]">real Products</span>{" "}
+          </div>
+          <div className="text-lg" style={{ fontFamily: "nothing" }}>
+            We make your ideas come to life with the latest technology and
+            assurred quality
+          </div>
+          <button
+            onClick={() => {
+              window.scrollTo({
+                top: document.getElementById("contact").offsetTop,
+                behavior: "smooth",
+              });
+            }}
+            className=" bg-[#f5e40b] py-3 px-10 rounded-md text-black text-lg"
+          >
+            ASK FOR QOUTE
+          </button>
+          <div className="pt-8 pl-16">
+            <Count />
+          </div>
         </div>
         <div className="w-[40%] border-solid  ">
-            <img src={vector} alt="" className='w-[500px] h-[500px]' />
+          <img src={vector} alt="" className="w-[500px] h-[500px]" />
         </div>
-
-        
-
-    </div>
-  </>
-  )
-}
+      </div>
+    </>
+  );
+};
 
 export default Featured;
